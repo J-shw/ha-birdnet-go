bcee4992fd0bcb7ae00babe70226a343956d2b4b
J-shw <91017208+J-shw@users.noreply.github.com>
2025-10-12 20:41:12 +0100

    Uses new API class to verify connection

--- custom_components/birdnet-go/__init__.py
+++ custom_components/birdnet-go/__init__.py
@@ -2,6 +2,7 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN, HOST, USERNAME, PASSWORD
+from .api import BirdnetGoApiClient
 
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,6 +16,16 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})[HOST] = host
     hass.data.setdefault(DOMAIN, {})[USERNAME] = username
     hass.data.setdefault(DOMAIN, {})[PASSWORD] = password
+
+    api_client = BirdnetGoApiClient(host, username, password)
+
+    try:
+        await api_client.async_get_streams_status()
+    except ConnectionError as err:
+        _LOGGER.error("Could not connect to API: %s", err)
+        return False
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = api_client
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
